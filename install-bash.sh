fed8ecb45216e60957984b03ff67655d8be008aa
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-05-26 14:16:12 +0700

    Update install-bash.sh

--- install-bash.sh
+++ install-bash.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 curl -sS https://starship.rs/install.sh | sh -s -- -f
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
 eval "$(starship init bash)"
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
